987b3bb685ae8b729511d6a413e857ce87fe6aa0
Divine Obinali <divinewilson766@gmail.com>
2024-11-24 11:45:40 +0100

    updated news feed for other news

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
 
   const [feeds, setFeeds] = useState([]);
 
+  const [category, setCategory] = useState('home');
+
   const getFeeds = async () => {
     try {
-      const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=c1oHaOp76tqA5cwuOxJSs9fpH5dSUuPA`);
+      const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=c1oHaOp76tqA5cwuOxJSs9fpH5dSUuPA`);
       const json = await response.json();
       setFeeds(json.results);
     } catch (err) {
@@ -19,22 +21,30 @@ function App() {
 
   useEffect(() => {
     getFeeds()
-  }, [])
+  }, [category])
+
+
+  const changeCategory = (newCategory) => {  
+    setCategory(newCategory);  
+  } 
 
 
   return (
     <>
-    <Header />
+    <Header changeCategory={ changeCategory } currentCategory = {category} />
     <div className='container'>
       <div className='row'>
     { feeds.map((feed) => {
       return (
-        <div className='col-lg-6 col-sm-4'>
-        <div className='mt-4 col-lg' key={feed.url}>
+        <div key={feed.url} className='col-lg-4 mt-4 card-container col-sm-4'>
+        <div className='col-lg'>
           <img className='img container-fluid' src={ feed.multimedia[0].url } alt={ feed.multimedia[0].caption}/>
           <h4 className='text-light p-3 font-weight'>{feed.title}</h4>
           <p className='p-3 text-secondary'>
             {feed.abstract}
+          </p>
+          <p className='text-light p-3'>
+            {feed.item_type} {feed.byline}
           </p>
         </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 
-const Header = () => {
+const Header = ({changeCategory, currentCategory}) => {
     return (
         <>
         <div className="container mt-4">
             <nav class="navbar navbar-expand-sm navbar-dark">
             <div class="container-fluid">
                 <h1 className="display-1  font-weight-bolder"><a class="text-light text-decoration-none" href="#">
-                    <b>Art Feeds</b>    
+                    <b>{ `${currentCategory.charAt(0).toUpperCase() + currentCategory.slice(1)}  Feeds`}</b>  
+                  
                 </a></h1>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
@@ -16,16 +17,16 @@ const Header = () => {
                 <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                    <a class="nav-link active" href="#">Arts</a>
+                    <a class="nav-link" onClick={() => changeCategory('home')}  href="#">For you</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Science</a>
+                    <a class="nav-link active" onClick={() => changeCategory('arts')} href="#">Arts</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">World</a>
+                    <a class="nav-link" onClick={() => changeCategory('us')} href="#">United states</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" onClick={() => changeCategory('world')} href="#">World</a>
                     </li>
                 </ul>
                 </div>
